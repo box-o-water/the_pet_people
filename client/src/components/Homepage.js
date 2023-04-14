a102b3786dd99f245476ea708c0cc75399ea79ba
Tyler R <117325916+Tyloneitor@users.noreply.github.com>
2023-04-13 20:56:34 -0400

    Page Development

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -4,11 +4,14 @@ function Homepage() {
   return (
     <div className="Homepage">
       <div id="Avail_users">
-      <div id="Avail_pets">
+        Renters
+        
         
       </div>
-
+      <div id="Avail_pets">
+        Their Pets
     </div>
+
 
 
     </div>
