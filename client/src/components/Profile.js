ce04d78b5a942e842b6b2c2f7975fae344627fea
box-o-water <boxooowater@gmail.com>
2023-04-15 21:18:09 -0500

    feat(display-pets): on the profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -8,6 +8,7 @@ import Auth from "../utils/auth";
 const Profile = () => {
   // use useQuery to get logged in user's data
   const { loading, data: userData } = useQuery(GET_ME);
+  const pets = userData?.me.pets || [];
 
   const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -21,10 +22,10 @@ const Profile = () => {
 
   return (
     <div>
-    <a href="/update-profile"> Update Profile</a>
-    <div>
-    <a href="/add-pet">Add Pet</a>
-    </div>
+      <a href="/update-profile"> Update Profile</a>
+      <div>
+        <a href="/add-pet">Add Pet</a>
+      </div>
 
       <h2>profile</h2>
       <p>Name: {userData?.me.username}</p>
@@ -35,31 +36,27 @@ const Profile = () => {
         alt="cat lady"
       ></img>
       <p>Location (City, State): {userData?.me.location}</p>
-      <h2>pets:</h2>
-      <p>pet1</p>
-      <p>pet2</p>
+      <div>
+        <h3>pets are people, too</h3>
+        {pets &&
+          pets.map((pet) => (
+            <div key={pet._id} className="card mb-3">
+              <h4 className="card-header bg-primary text-light p-2 m-0">
+                {pet.petName}
+              </h4>
+              <div className="card-body bg-light p-2">
+                <p>{pet.animalType}</p>
+                <p>{pet.breed}</p>
+                <p>{pet.size}</p>
+              </div>
+            </div>
+          ))}
+      </div>
       <h2>reviews:</h2>
       <p>Review Title:</p>
       <p>review body</p>
     </div>
   );
 };
-
-// function Profile() {
-//   return (
-//     <div>
-//       <h2>profile</h2>
-//       <img src="https://i.guim.co.uk/img/media/e4ae055cd7e0b946e216e2a43a97fcf085c364e6/463_41_2032_1219/master/2032.jpg?width=645&quality=45&dpr=2&s=none" width="500" alt="cat lady"></img>
-//       <p>Name:</p>
-//       <p>Location (City, State):</p>
-//       <h2>pets:</h2>
-//       <p>pet1</p>
-//       <p>pet2</p>
-//       <h2>reviews:</h2>
-//       <p>Review Title:</p>
-//       <p>review body</p>
-//     </div>
-//   );
-// }
 
 export default Profile;
