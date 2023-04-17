d32b33569f8b9c5984bc8d79d13e099cb5734633
Carlee Miller <carlee.miller@gmail.com>
2023-04-17 11:36:00 -0500

    finished styling on UsersList

--- client/src/components/UsersList.js
+++ client/src/components/UsersList.js
@@ -16,13 +16,13 @@ const UsersList = () => {
       <h3 className="ml-4 mt-2 mr-4 mb-2 font-bold text-lg">renters are people, too</h3>
       {users &&
         users.map((user) => (
-          <div className="max-w-sm w-full lg:max-w-full lg:flex m-2 shadow-lg m-4">
+          <div className="max-w-sm w-full lg:max-w-full lg:flex m-2 shadow-lg m-4 rounded-md">
             {/* <img
               className="h-44 lg:h-auto lg:w-44 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden border-solid border-slate-300"
               src= { corgibutt }
               alt="illustrated corgi butt"
             ></img> */}
-            <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal w-full">
+            <div className="border-r border-b border-l border-pink-500 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal w-full">
             <div className="mb-8">
             <p className="text-sm text-gray-600 flex items-center"></p>
             <div className="text-gray-900 font-bold text-xl mb-2">
